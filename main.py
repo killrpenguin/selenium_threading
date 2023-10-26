8f498fbe46c0edea96d920ab4c62b4eb41a071c7
killrpenguin <dmcfarland8031@gmail.com>
2023-10-25 18:19:56 -0600

    testing threadpoolexecutor

---
+++ main.py
@@ -0,0 +1,42 @@
+import concurrent.futures as cf
+import time
+import asyncio
+import selenium
+import seleniumwire
+from seleniumwire import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.by import By
+from seleniumwire.webdriver import EdgeOptions
+
+
+class Driver_Adapter(webdriver.Edge):
+    def __init__(self, link, *args, **kwargs):
+        edge_options = EdgeOptions()
+        edge_options.use_chromium = True
+        edge_options.add_argument("start-maximized")
+        edge_options.page_load_strategy = "eager"
+        edge_options.add_argument("disable-gpu")
+        edge_options.add_argument("headless")
+        edge_options.add_argument("--proxy_server=%s" % "http://24.158.29.166:80")
+        super().__init__(edge_options=edge_options)
+        self.link = link
+        self.start_time = time.perf_counter()
+        self.valid_resps = [200, 301, 302, 307, 404]
+
+
+def make_driver(link, program_start) -> webdriver:
+    driver = Driver_Adapter(link)
+    driver.get(driver.link)
+    print(f"Adapter Created {driver.start_time - program_start:.2f}")
+    return driver
+
+
+with cf.ThreadPoolExecutor(max_workers=16) as executor:
+    with open("blue_farm_links", "r") as file:
+        program_start = time.perf_counter()
+        test_links = file.read().strip().split('\n')
+        futures = [executor.submit(make_driver(link, program_start)) for link in test_links]
+        successful_drivers = [future.result() for future in cf.as_completed(futures) if
+                              future.result().request.response.status_code in future.result().valid_resps]
+        print(successful_drivers)
